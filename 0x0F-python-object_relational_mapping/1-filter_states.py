2d3af43c43f16487f23398bb23d5be8b331c5f53
marcrine-geek <marcrinemm@gmail.com>
2022-09-09 13:26:40 +0300

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+This script lists all states with letter N
+database `hbtn_0e_0_usa`.
+"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
