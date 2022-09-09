93a29cdd8ef9d15296aa4de3bcd68c56dfea4ee2
marcrine-geek <marcrinemm@gmail.com>
2022-09-09 15:16:56 +0300

    task 1 checks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the states
-    from the database.
-    """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
