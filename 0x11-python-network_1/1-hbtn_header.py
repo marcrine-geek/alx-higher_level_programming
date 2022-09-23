bcb6246e587250d906ae15fc0a9df585fa77f1f2
marcrine-geek <marcrinemm@gmail.com>
2022-09-23 16:41:35 +0300

    response header fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL>
+Usage: ./1-hbtn_header.py URL
 """
+
 import sys
 import urllib.request
 
